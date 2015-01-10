40ab3ef39d9883ea839c18ca8adb65c76fac1a8c
Antonio Maestre Rosa <i12maroa@uco.es>
2015-01-10 19:53:36 +0100

    Updated gestorDBFichero.cpp, fixed e-mail values

--- src/gestorDBFichero.cpp
+++ src/gestorDBFichero.cpp
@@ -22,22 +22,26 @@ using namespace std;
 void guardar(const vector<Contacto> &_contactos,string fichero)
 {
 	vector<Contacto>::const_iterator it; //Declaración de iterador const_iterator it para recorrer el vector
+	
+	//Declaración del flujo de salida
 	ofstream flujoSalida;
+	//Se abre el fichero para escribir
+	flujoSalida.open(fichero.c_str(),ios::out);
 	
-	flujoSalida.open(fichero.c_str(),ios::out);//Se abre el fichero para escribir
-	//File * flujoSalida;
-	
-	if(flujoSalida) //El fichero se ha abierto correctamente
+	//El fichero se ha abierto correctamente
+	if(flujoSalida)
 	{
 		for (it=_contactos.begin();it!=_contactos.end() ; it++) { //Recorremos el vector
-			flujoSalida<<it->getNombre()<<endl;  //Guardamos cada contacto en el fichero
-			flujoSalida<<it->getApellidos()<<endl;
-			flujoSalida<<it->getTelefono()<<endl;
-			flujoSalida<<it->getDni()<<endl;
-			flujoSalida<<espacio2caracter(it->getDireccion())<<" \n";
+			
+			//Guardamos cada contacto en el fichero
+			flujoSalida<<it->getNombre()<<endl;		//Nombre
+			flujoSalida<<it->getApellidos()<<endl;		//Apellidos
+			flujoSalida<<it->getTelefono()<<endl;		//Telefono
+			flujoSalida<<it->getDni()<<endl;		//DNI
+			flujoSalida<<it->getEmail()<<endl;		//E-mail
+			flujoSalida<<espacio2caracter(it->getDireccion())<<endl;	//Direccion
 
-			//flujoSalida<<it->getDireccion()<<endl;
-			//flujoSalida<<endl<<"---------------------------"<<endl;
+			
 			
 			
 		}
@@ -52,48 +56,42 @@ void guardar(const vector<Contacto> &_contactos,string fichero)
 
 void cargar(vector<Contacto> & contactos, string fichero)
 {
-	//vector<Contacto>::iterator it;
 	Contacto aux;
 	ifstream flujoEntrada; //Declaración del flujo de entrada
 	flujoEntrada.open(fichero.c_str(),ios::in);//Se abre el fichero para leer
-	string nombre,apellido1,apellido2,apellidos,dni,telefono,direccionsF,direccion;
-	//int dni,telefono;
+	string nombre,apellido1,apellido2,apellidos,dni,telefono,direccionsF,direccion,email;
+	
 	
 	if(flujoEntrada)
 	{
-		//Se lee el primero
-		//flujoEntrada!=flujoEntrada.feof()
-		//flujoEntrada.getline(nombre, sizeof(string));
 		while (flujoEntrada)//Mientras no se llegue al final del fichero
-		//while(flujoEntrada)
 		{
 			flujoEntrada>>nombre;
-			//flujoEntrada.getline(apellidos,100,'\n');
-			//getchar();
 			flujoEntrada>>apellido1;
 			flujoEntrada>>apellido2;
-			//getline(flujoEntrada, apellidos);
-			//cout<<"Apellidos"<<apellidos<<endl;
 			flujoEntrada>>telefono;
 			flujoEntrada>>dni;
+			flujoEntrada>>email;
 			flujoEntrada>>direccionsF;
-			//flujoEntrada>>
-			direccion=caracter2espacio(direccionsF);
-			//cout<<"Direccion:"<<direccion<<endl;
+			//flujoEntrada.getline(direccionsF,30);
 			
+			//Filtramos la direccion para quitar los '_' y poner ' '.
+			//direccion=caracter2espacio(direccionsF);
+			//Los apellidos serán el apellido1 y el apellido 2
 			apellidos=apellido1+" "+apellido2;
-			
+			//Introducimos los datos en el objeto de tipo Contacto aux
 			aux.setNombre(nombre);
 			aux.setApellidos(apellidos);
 			aux.setTelefono(telefono);
 			aux.setDni(dni);
 			aux.setDireccion(direccion);
+			aux.setEmail(email);
+			//Insertamos el contacto leido en el vector
+			contactos.push_back(aux);
 			
-			contactos.push_back(aux);   //Se inserta el contacto leido en el vector
-			//flujoEntrada.read((char *) (&aux), sizeof(Contacto));  //Se lee el siguiente
 		}
-		
-		flujoEntrada.close();  //Cerramos el fichero
+		//Cerramos el fichero
+		flujoEntrada.close();
 	}
 }
 
